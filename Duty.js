59ce79f1c651cfa4c934063c0286dd26baa09532
LiamOrmiston <ljormiston@ku.edu>
2017-01-22 22:51:00 -0600

    Created a .js file to keep my code more organized. I will implement it once I learn more about Javascript

---
+++ Duty.js
@@ -0,0 +1,225 @@
+/*
+This is code is the same that is in index.html
+I am keeping this file but it wont be used until I can
+become more familiar with Javascript working with html.
+*/
+
+var Day = new Date().getDay();
+var time = new Date().getHours();
+var name = "default";
+switch (Day)
+{
+  case 0:
+    document.write(name + " has the phone!");
+    break;
+  case 1:
+    switch (time)
+    {
+      case 7:
+        name = "Sofia"
+        break;
+      case 8:
+        name = "Ryan"
+        break;
+      case 9:
+        name = "Ryan"
+        break;
+      case 10:
+        name = "Riya"
+        break;
+      case 11:
+        name = "Riya"
+        break;
+      case 12:
+        name = "Alyssa"
+        break;
+      case 13:
+        name = "Danielle"
+        break;
+      case 14:
+        name = "Danielle"
+        break;
+      case 15:
+        name = "Jackie"
+        break;
+      case 16:
+        name = "Jackie"
+        break;
+      case 17:
+        name = "Jackie"
+        break;
+      case 18:
+        name = "Ben"
+        break;
+    }
+    document.write(name+" has the phone!");
+    break;
+  case 2:
+  switch (time)
+  {
+    case 7:
+      name = "Sofia"
+      break;
+    case 8:
+      name = "Riya"
+      break;
+    case 9:
+      name = "Ben"
+      break;
+    case 10:
+      name = "Ryan"
+      break;
+    case 11:
+      name = "Sander"
+      break;
+    case 12:
+      name = "Sander"
+      break;
+    case 13:
+      name = "Sander"
+      break;
+    case 14:
+      name = "Liam"
+      break;
+    case 15:
+      name = "Danielle"
+      break;
+    case 16:
+      name = "Danielle"
+      break;
+    case 17:
+      name = "Danielle"
+      break;
+    case 18:
+      name = "Ryan"
+      break;
+  }
+    document.write(name + " has the phone!");
+    break;
+  case 3:
+  switch (time)
+  {
+    case 7:
+      name = "Sofia"
+      break;
+    case 8:
+      name = "Michael"
+      break;
+    case 9:
+      name = "Michael"
+      break;
+    case 10:
+      name = "Michael"
+      break;
+    case 11:
+      name = "Riya"
+      break;
+    case 12:
+      name = "Alyssa"
+      break;
+    case 13:
+      name = "Jesse"
+      break;
+    case 14:
+      name = "Jesse"
+      break;
+    case 15:
+      name = "Jesse"
+      break;
+    case 16:
+      name = "Jesse"
+      break;
+    case 17:
+      name = "Jesse"
+      break;
+    case 18:
+      name = "Sofia"
+      break;
+  }
+    document.write(name + " has the phone!");
+    break;
+  case 4:
+  switch (time)
+  {
+    case 7:
+      name = "Josh"
+      break;
+    case 8:
+      name = "Josh"
+      break;
+    case 9:
+      name = "Josh"
+      break;
+    case 10:
+      name = "Ryan"
+      break;
+    case 11:
+      name = "Sander"
+      break;
+    case 12:
+      name = "Sander"
+      break;
+    case 13:
+      name = "Alyssa"
+      break;
+    case 14:
+      name = "Alyssa"
+      break;
+    case 15:
+      name = "Alyssa"
+      break;
+    case 16:
+      name = "Ben"
+      break;
+    case 17:
+      name = "Ben"
+      break;
+    case 18:
+      name = "Ben"
+      break;
+  }
+    document.write(name + " has the phone!");
+    break;
+  case 5:
+  switch (time)
+  {
+    case 7:
+      name = "Liam"
+      break;
+    case 8:
+      name = "Liam"
+      break;
+    case 9:
+      name = "Liam"
+      break;
+    case 10:
+      name = "Michael"
+      break;
+    case 11:
+      name = "Liam"
+      break;
+    case 12:
+      name = "Riya"
+      break;
+    case 13:
+      name = "Josh"
+      break;
+    case 14:
+      name = "Josh"
+      break;
+    case 15:
+      name = "Jackie"
+      break;
+    case 16:
+      name = "Jackie"
+      break;
+    case 17:
+      name = "Michael"
+      break;
+    case 18:
+      name = "Sofia"
+      break;
+  }
+    document.write(name + " has the phone!");
+    break;
+}
